12c7135b55f301b42e05edac97c12844f8add6b3
Abdulaziz Nal <abdulaziz.nal@anywai.ch>
2022-02-12 21:40:10 +0300

    setup custom menu implementation for chat fragment

--- app/src/main/java/com/example/whatsappclone/chat/ChatFragment.kt
+++ app/src/main/java/com/example/whatsappclone/chat/ChatFragment.kt
@@ -2,12 +2,14 @@ package com.example.whatsappclone.chat
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.example.whatsappclone.R
 import com.example.whatsappclone.databinding.FragmentChatBinding
+
+private const val TAG = "ChatFragment"
 
 class ChatFragment : Fragment() {
 
@@ -24,11 +26,25 @@ class ChatFragment : Fragment() {
 
         binding.lifecycleOwner = this
 
-        binding.centeredText.text = "I should load the chat with id ${args.chatId.toString()}, but I don't have a database yet!"
+        binding.centeredText.text =
+            "I should load the chat with id ${args.chatId.toString()}, but I don't have a database yet!"
+
+        setHasOptionsMenu(true)
 
         bindViewModel()
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_chat, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_block_button -> false
+            else -> false
+        }
     }
 
     private fun bindViewModel() {
