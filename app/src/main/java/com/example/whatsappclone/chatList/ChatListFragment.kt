8b8872ea28ef8ac03fd1819f0aab6981091f909b
aziznal <aziznal.dev@gmail.com>
2022-02-21 21:07:42 +0300

    introduce single shot event and refactor

--- app/src/main/java/com/example/whatsappclone/chatList/ChatListFragment.kt
+++ app/src/main/java/com/example/whatsappclone/chatList/ChatListFragment.kt
@@ -1,25 +1,24 @@
 package com.example.whatsappclone.chatList
 
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.whatsappclone.R
 import com.example.whatsappclone.databinding.FragmentChatListBinding
 
 private const val TAG = "ChatListFragment"
 
-
 class ChatListFragment : Fragment() {
-
     private var _binding: FragmentChatListBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var viewModel: ChatListFragmentViewModel
+
+    private val chatAdapter by lazy {
+        createChatListAdapter()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,15 +38,36 @@ class ChatListFragment : Fragment() {
         return binding.root
     }
 
+    private fun createChatListAdapter() = ChatListAdapter(MockChatData().chatList) {
+        onListedChatClicked(it)
+    }
+
+    private fun onListedChatClicked(it: ChatItem) {
+        findNavController().navigate(
+            ChatListFragmentDirections.actionChatListFragmentToChatFragment(
+                it.chatName
+            )
+        )
+    }
+
     private fun handleViewModelObservables() {
-        viewModel.gotoNewChatScreenEvent.observe(
+        viewModel.eventEmitter.observe(
             viewLifecycleOwner
         ) { eventValue ->
-            if (eventValue) {
-                findNavController().navigate(R.id.action_chatListFragment_to_newChatFragment)
-                viewModel.completeGotoNewChatScreenEvent()
+            when (eventValue) {
+                is ChatListFragmentViewModel.Events.GotoNewChatScreenEvent -> {
+                    gotoNewChatScreen()
+                }
+
+                else -> {}
             }
         }
+    }
+
+    private fun gotoNewChatScreen() {
+        findNavController().navigate(
+            ChatListFragmentDirections.actionChatListFragmentToNewChatFragment()
+        )
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -55,17 +75,6 @@ class ChatListFragment : Fragment() {
     }
 
     private fun setupChatList() {
-        val chatAdapter = ChatListAdapter(MockChatData().chatList) {
-            findNavController().navigate(
-                ChatListFragmentDirections.actionChatListFragmentToChatFragment(
-                    "123456"
-                )
-            )
-        }
-
-        val layoutManager = LinearLayoutManager(context)
-
-        binding.chatList.layoutManager = layoutManager
         binding.chatList.adapter = chatAdapter
     }
 
--- app/src/main/java/com/example/whatsappclone/chatList/ChatListFragmentViewModel.kt
+++ app/src/main/java/com/example/whatsappclone/chatList/ChatListFragmentViewModel.kt
@@ -1,27 +1,27 @@
 package com.example.whatsappclone.chatList
 
+import SingleShotEvent
 import android.util.Log
+import androidx.annotation.MainThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.concurrent.atomic.AtomicBoolean
 
 private const val TAG = "ChatFragmentViewModel"
 
 class ChatListFragmentViewModel : ViewModel() {
 
-    private var _gotoNewChatScreenEvent = MutableLiveData(false)
-    public val gotoNewChatScreenEvent: LiveData<Boolean>
-        get() = _gotoNewChatScreenEvent
+    private var _eventEmitter = SingleShotEvent<Events>()
+    val eventEmitter: LiveData<Events> get() = _eventEmitter
 
     fun onNewChatButtonClicked() {
         Log.d(TAG, "onNewChatButtonClicked: Clicked Fab")
-        _gotoNewChatScreenEvent.value = true
+
+        _eventEmitter.setValue(Events.GotoNewChatScreenEvent)
     }
 
-
-    fun completeGotoNewChatScreenEvent() {
-        Log.d(TAG, "onNewChatButtonClicked: Handled Fab Click")
-        _gotoNewChatScreenEvent.value = false
+    sealed class Events {
+        object GotoNewChatScreenEvent : Events()
     }
-
 }
